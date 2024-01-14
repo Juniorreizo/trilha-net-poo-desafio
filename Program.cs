0a6b8bc48188c901b8d84f0411b00a7a2286ed3e
Juniorreizo <155012583+Juniorreizo@users.noreply.github.com>
2024-01-14 18:38:56 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
-﻿using DesafioPOO.Models;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Smartphone Nokia: ");
+        Nokia nokia = new Nokia("999999999", "Modelo NokiaS", "123456", 64);
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("Whatsapp");
+
+        Console.WriteLine("\n");
+
+        Console.WriteLine("Smartphone Iphone: ");
+        Iphone iphone = new Iphone("988888888", "Iphone 6S", "654321", 3);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+
+    }
+}
